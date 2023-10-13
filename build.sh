c0bc2ae34c3ab2cd5352239caff8dc9147cc9f37
Michele Salerno <mikysal78@gmail.com>
2023-10-13 20:57:02 +0200

    switch current stable v23.05.0

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 TARGET=$1
-OPENWRT_VERSION="v23.05.0-rc4"
+OPENWRT_VERSION="v23.05.0"
 
 ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BUILD_DIR="/mnt/nfs-firmware/${OPENWRT_VERSION}/${TARGET}"
