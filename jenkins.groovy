48d641dbc678f4a0b51d3f7105dd8e2181012c4e
Michele Salerno <mikysal78@gmail.com>
2024-05-25 11:01:06 +0200

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -21,17 +21,17 @@ def publishArtifact(UPLOAD_FILE) {
 
 def githubRelease(UPLOAD_FILE, ARCHIVE_NAME) {
     //Upload on github if tag
-    sh "git tag --contains `git rev-parse HEAD` > .git-tag"
+    sh "git tag --contains "$(git rev-parse HEAD)" > .git-tag"
     def GIT_TAG = readFile('.git-tag').trim()
     sh "rm .git-tag"
     stage('Publish github release') {
         if (!GIT_TAG) {
             Utils.markStageSkippedForConditional(STAGE_NAME)
         } else {
-              //withCredentials([[$class: 'StringBinding', credentialsId: 'GithubToken', variable: 'GITHUB_TOKEN']]) {
-              withCredentials([usernamePassword(credentialsId: 'GitHubApp',
-                                  usernameVariable: 'GITHUB_APP',
-                                  passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
+              withCredentials([[$class: 'StringBinding', credentialsId: 'GithubToken', variable: 'GITHUB_TOKEN']]) {
+              //withCredentials([usernamePassword(credentialsId: 'GitHubApp',
+              //                    usernameVariable: 'GITHUB_APP',
+              //                    passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
               sh "github-release info -u mikysal78 -r ninux-build-openwrt -t ${GIT_TAG} || github-release release -u mikysal78 -r ninux-build-openwrt -t ${GIT_TAG}"
               sh "github-release upload -u mikysal78 -r ninux-build-openwrt -t ${GIT_TAG} -n ${ARCHIVE_NAME} -f ${UPLOAD_FILE}"
             }
