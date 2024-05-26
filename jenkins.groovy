96b25f279ea0184dc55ceae76a24d6af0e4b818b
Michele Salerno <mikysal78@gmail.com>
2024-05-27 00:30:45 +0200

    test

--- jenkins.groovy
+++ jenkins.groovy
@@ -28,10 +28,10 @@ def githubRelease(UPLOAD_FILE, ARCHIVE_NAME) {
         if (!GIT_TAG) {
             Utils.markStageSkippedForConditional(STAGE_NAME)
         } else {
-              //withCredentials([[$class: 'StringBinding', credentialsId: 'GithubToken', variable: 'GITHUB_TOKEN']]) {
-              withCredentials([usernamePassword(credentialsId: 'GitHubApp',
-                                  usernameVariable: 'GITHUB_APP',
-                                  passwordVariable: 'GITHUB_TOKEN')]) {
+              withCredentials([[$class: 'StringBinding', credentialsId: 'GithubToken', variable: 'GITHUB_TOKEN']]) {
+              //withCredentials([usernamePassword(credentialsId: 'GitHubApp',
+              //                    usernameVariable: 'GITHUB_APP',
+              //                    passwordVariable: 'GITHUB_TOKEN')]) {
               sh "github-release info -u mikysal78 -r ninux-build-openwrt -t ${GIT_TAG} || github-release release -u mikysal78 -r ninux-build-openwrt -t ${GIT_TAG}"
               sh "github-release upload -u mikysal78 -r ninux-build-openwrt -t ${GIT_TAG} -n ${ARCHIVE_NAME} -f ${UPLOAD_FILE}"
             }
